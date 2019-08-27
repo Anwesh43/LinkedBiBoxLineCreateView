b14f3431e16543650b877d0c9f83f21bb0744599
Anwesh43 <anweshthecool0@gmail.com>
2019-08-27 10:16:54 +0530

    adding libraries and constants

--- biboxlinecreateview/src/main/java/com/anwesh/uiprojects/biboxlinecreateview/BiBoxLineCreateView.kt
+++ biboxlinecreateview/src/main/java/com/anwesh/uiprojects/biboxlinecreateview/BiBoxLineCreateView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.biboxlinecreateview
  * Created by anweshmishra on 27/08/19.
  */
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#1A237E")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
