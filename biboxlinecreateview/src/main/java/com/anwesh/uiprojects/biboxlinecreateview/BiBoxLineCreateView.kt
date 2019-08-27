07ecbba35340a82d5bb88b4c4d015786f19743fa
Anwesh43 <anweshthecool0@gmail.com>
2019-08-27 10:32:39 +0530

    adding BiBoxLineCreateView class

--- biboxlinecreateview/src/main/java/com/anwesh/uiprojects/biboxlinecreateview/BiBoxLineCreateView.kt
+++ biboxlinecreateview/src/main/java/com/anwesh/uiprojects/biboxlinecreateview/BiBoxLineCreateView.kt
@@ -71,3 +71,21 @@ fun Canvas.drawBBLCNode(i : Int, scale : Float, paint : Paint) {
     drawBiBoxLine(sc1, sc2, size, h, paint)
     restore()
 }
+
+class BiBoxLineCreateView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
